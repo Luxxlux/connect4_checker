b4c9baa0d67c65802792288a8713bc8e30433d28
Luxxlux <laimislux@outlook.com>
2024-07-21 20:33:52 +0100

    Add files via upload
    Grabbing information from the main file

---
+++ mainfile.py
@@ -0,0 +1,49 @@
+if __name__ == '__main__':
+    try:
+        f = open('text','r')
+
+        for line in f:
+            x = 0
+            x += 1
+            emp_list_1 = []
+            emp_list_2 = []
+            emp_list_3 = []
+            emp_list_4 = []
+            emp_list_5 = []
+            emp_list_6 = []
+
+            print(line, end='')
+            emp_list_1.append(line)
+
+
+
+
+
+
+
+
+
+
+        print(line)
+        print(x)
+
+
+
+
+
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        f.close()
+
+
+
+
+
+
+
+
+
+
